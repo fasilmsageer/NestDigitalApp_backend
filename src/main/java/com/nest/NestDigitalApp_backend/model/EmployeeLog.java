d7a3f3902a2cd3b2cb15f298e2b96bb6105fa291
fasilmsageer <fasilmsageer@gmail.com>
2022-12-12 12:42:02 +0530

    model

--- src/main/java/com/nest/NestDigitalApp_backend/model/EmployeeLog.java
+++ src/main/java/com/nest/NestDigitalApp_backend/model/EmployeeLog.java
@@ -1,4 +1,21 @@
 package com.nest.NestDigitalApp_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employeelog")
+
 public class EmployeeLog {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+    private int empid;
+    private String date;
+    private String entrytime;
+    private String exittime;
 }
--- src/main/java/com/nest/NestDigitalApp_backend/model/VisitorLog.java
+++ src/main/java/com/nest/NestDigitalApp_backend/model/VisitorLog.java
@@ -1,4 +1,26 @@
 package com.nest.NestDigitalApp_backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "visitorlog")
+
+
+
 public class VisitorLog {
+
+    @Id
+    @GeneratedValue
+
+    private int id;
+    private String name;
+    private String purpose;
+    private String whom_to_meet;
+    private String date;
+    private String entrytime;
+    private String exittime;
+
 }
